0f7b5a200a5f59e165e240d89524a42c0cb7d621
Juliano Fernandes <julianofernandes@gmail.com>
2019-05-18 21:56:44 -0300

    add loan command
    - add command loan
    - initialize conversation for a new loan
    - minor code refactory

--- bot/loanbot.py
+++ bot/loanbot.py
@@ -1,6 +1,13 @@
 import logging
+from enum import Enum, auto
 
-from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
+from telegram.ext import (
+    CommandHandler,
+    ConversationHandler,
+    Filters,
+    MessageHandler,
+    Updater,
+)
 
 from bot import settings
 
@@ -11,35 +18,56 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+class LoanState(Enum):
+    GETCLIENT = auto()
+
+
 def start(update, context):
-    context.bot.send_message(
-        chat_id=update.message.chat_id,
-        text=(
-            "Hello. How can I help you? A loan maybe?\n\n"
+    update.message.reply_text(
+        (
+            "Hello. How can I help you? A loan maybe?\n"
             "If you're not sure type /help for the available command list."
-        ),
+        )
     )
 
 
 def help(update, context):
-    context.bot.send_message(
-        chat_id=update.message.chat_id,
-        text=("/start - show greeting message\n" "/help - show command list"),
+    update.message.reply_text(
+        (
+            "/start - show greeting message\n"
+            "/loan - create a loan\n"
+            "/cancel - cancel current operation\n"
+            "/help - show command list"
+        )
     )
 
 
 def unknown(update, context):
-    context.bot.send_message(
-        chat_id=update.message.chat_id,
-        text=(
-            "Sorry, I didn't understand that command. "
+    update.message.reply_text(
+        (
+            "Sorry, I didn't understand that command.\n"
             "Try /help to see the list of available commands."
-        ),
+        )
     )
 
 
 def error(update, context):
     logger.warning("Update '%s' caused error '%s'", update, context.error)
+
+
+def loan(update, context):
+    update.message.reply_text("Please, send me your CPF number.")
+    return LoanState.GETCLIENT
+
+
+def get_client(update, context):
+    update.message.reply_text(update.message.text)
+    return ConversationHandler.END
+
+
+def cancel(update, context):
+    update.message.reply_text("Ok, no problem, come back whenever you want.")
+    return ConversationHandler.END
 
 
 def main():
@@ -48,6 +76,15 @@ def main():
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(
+        ConversationHandler(
+            entry_points=[CommandHandler("loan", loan)],
+            states={
+                LoanState.GETCLIENT: [MessageHandler(Filters.text, get_client)]
+            },
+            fallbacks=[CommandHandler("cancel", cancel)],
+        )
+    )
     dp.add_handler(MessageHandler(Filters.command, unknown))
     dp.add_error_handler(error)
 
